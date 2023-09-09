cd837a6455f324687790e4cc1170b8943ffdbd28
Mohamed Mahmoud <m.raslan97@gmail.com>
2023-09-09 23:43:40 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_course_aug23_asyncronous_programming_and_api_http_requests/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_course_aug23_asyncronous_programming_and_api_http_requests
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
