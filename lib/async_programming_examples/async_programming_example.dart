75a0444d0b719bd3192a607b26cb42af76cc947e
Mohamed Mahmoud <m.raslan97@gmail.com>
2023-09-10 15:41:30 +0300

    added async programming example

--- lib/async_programming_examples/async_programming_example.dart
+++ lib/async_programming_examples/async_programming_example.dart
@@ -8,8 +8,48 @@ class AsyncProgrammingExample extends StatefulWidget {
 }
 
 class _AsyncProgrammingExampleState extends State<AsyncProgrammingExample> {
+
+
+  void func(){
+     Future.delayed(const Duration(seconds: 3), (){
+      print("السلام عليكم");
+    });
+
+    Future.delayed(const Duration(seconds: 2), (){
+      print("وعليكم السلام");
+    });
+
+    print("أنا عاطف معاكوا هنا");
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    func();
+
+    print("أنا مليش دعوة بيهم");
+
+  }
+
+
+  void func2()async{
+    String username = await Future.delayed(const Duration(seconds: 3), (){
+     return "mohamed";
+    });
+
+
+    String password = await Future.delayed(const Duration(seconds: 2), (){
+      return "1234";
+    });
+
+
+    print("$username - $password");
+  }
+
   @override
   Widget build(BuildContext context) {
+
+
     return const Scaffold(
 
     );
---
+++ lib/http_examples/http_example1.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HttpExample1 extends StatefulWidget {
+  const HttpExample1({Key? key}) : super(key: key);
+
+  @override
+  State<HttpExample1> createState() => _HttpExample1State();
+}
+
+class _HttpExample1State extends State<HttpExample1> {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
